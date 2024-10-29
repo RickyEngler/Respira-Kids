23a8ed5ebe1f8b52e30481c85741c3de568c14b3
Augusto Henrique Engler <144976023+RickyEngler@users.noreply.github.com>
2024-10-29 12:37:05 -0300

    Database Running

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
-const express = require('express');
-const path = require('path');
-const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('usuarios.db');
+import express from 'express';
+import sqlite3 from 'sqlite3';
+import { open } from 'sqlite';
+import bodyParser from 'body-parser';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import bcrypt from 'bcrypt';
 
 
 const app = express();
 const PORT = 3000;
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 // Configurar a pasta public para servir arquivos estáticos
+app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Middleware para processar dados do formulário
@@ -16,10 +23,28 @@ app.use(express.json());
 
 // Rota principal (/) para exibir cadastro.html
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'views', 'cadastro.html'));
+  res.sendFile(path.join(__dirname, 'public', 'views', 'login.html'));
 });
 
-app.post('/cadastro', (req, res) => {
+async function criarEpopularTabelaDeUsuarios(nome_completo, email, telefone, data_nascimento, senha) {
+  const db = await open({
+    filename: './public/database/usuarios.db',
+    driver: sqlite3.Database,
+  });
+
+  // Hash a senha antes de armazená-la
+  const saltRounds = 10;
+  const hashedPassword = await bcrypt.hash(senha, saltRounds);
+
+  await db.run('CREATE TABLE IF NOT EXISTS usuarios (nome_completo varchar(30) NOT NULL, email varchar(100) NOT NULL UNIQUE, telefone varchar(14) NOT NULL PRIMARY KEY UNIQUE, data_nascimento varchar(40) NOT NULL, senha varchar(100) NOT NULL)');
+  
+  await db.run('INSERT INTO usuarios (nome_completo, email, telefone, data_nascimento, senha) VALUES (?, ?, ?, ?, ?)', [nome_completo, email, telefone, data_nascimento, hashedPassword]);
+
+  console.log('Usuário cadastrado com sucesso!');
+}
+
+
+app.post('/cadastro', async (req, res) => {
   const { nome_completo, email, telefone, data_nascimento, senha } = req.body;
 
   // Verifique se todos os campos obrigatórios foram enviados
@@ -27,16 +52,15 @@ app.post('/cadastro', (req, res) => {
     return res.status(400).send("Todos os campos são obrigatórios!");
   }
 
-  // Inserir os dados na tabela de usuários
-  const query = `INSERT INTO usuarios (nome_completo, email, telefone, data_nascimento, senha) VALUES (?, ?, ?, ?, ?)`;
-  db.run(query, [nome_completo, email, telefone, data_nascimento, senha], function(err) {
-    if (err) {
-      console.error("Erro ao salvar o usuário:", err);
-      return res.status(500).send("Erro ao realizar o cadastro");
-    }
+  try {
+    await criarEpopularTabelaDeUsuarios(nome_completo, email, telefone, data_nascimento, senha);
     res.send('Cadastro realizado com sucesso!');
-  });
+  } catch (err) {
+    console.error("Erro ao realizar o cadastro:", err);
+    return res.status(500).send("Erro ao realizar o cadastro");
+  }
 });
+
 
 
 // Iniciar o servidor
